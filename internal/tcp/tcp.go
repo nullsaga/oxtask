2d3d47d4a08845b7e369d0bf0c34e9c783ce1825
nullsaga <9040460+nullsaga@users.noreply.github.com>
2025-07-04 07:05:21 +0300

    implement tcp broadcasting

--- internal/tcp/tcp.go
+++ internal/tcp/tcp.go
@@ -1,6 +1,8 @@
 package tcp
 
 import (
+	"bufio"
+	"fmt"
 	"net"
 	"sync"
 )
@@ -36,6 +38,66 @@ func (s *Server) Serve() error {
 		s.mutex.Lock()
 		s.clients[c] = struct{}{}
 		s.mutex.Unlock()
+
+		go s.handleClientConn(c)
+	}
+}
+
+func (s *Server) handleClientConn(c *client) {
+	defer c.conn.Close()
+	r := bufio.NewReader(c.conn)
+	go s.write(c)
+
+	for {
+		msg, err := r.ReadBytes('\n')
+		if err != nil {
+			break
+		}
+
+		c.incrementSentBytes(len(msg))
+		if c.isBandwidthLimitExceeded() {
+			_, _ = c.conn.Write([]byte("Disconnected due to exceeding bandwidth limit\n"))
+			break
+		}
+		s.broadcast(c, msg)
+	}
+
+	s.removeClient(c)
+}
+
+func (s *Server) write(c *client) {
+	for msg := range c.sendCh {
+		n, err := c.conn.Write(msg)
+		if err != nil {
+			fmt.Printf("write error to %s: %v\n", c.conn.RemoteAddr(), err)
+			break
+		}
+
+		c.incrementSentBytes(n)
+		if c.isBandwidthLimitExceeded() {
+			_, _ = c.conn.Write([]byte("Disconnected due to exceeding bandwidth limit\n"))
+			break
+		}
+	}
+
+	s.removeClient(c)
+}
+
+func (s *Server) broadcast(sender *client, msg []byte) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	for c := range s.clients {
+		if c == sender {
+			continue
+		}
+
+		select {
+		case c.sendCh <- msg:
+		default:
+			fmt.Printf("client %s too slow, disconnecting\n", c.conn.RemoteAddr())
+			s.removeClient(c)
+		}
 	}
 }
 
